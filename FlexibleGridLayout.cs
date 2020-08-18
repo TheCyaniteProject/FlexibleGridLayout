a99b6f8e6d10af12e7e848d7bc838f3c3e9eb3ef
Kieee Shadow <TheCyaniteProject@users.noreply.github.com>
2020-08-17 20:00:19 -0500

    Finally

---
+++ FlexibleGridLayout.cs
@@ -0,0 +1,80 @@
+﻿using System.Security.Cryptography;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexibleGridLayout : LayoutGroup 
+{
+    public enum FitType {
+        Uniform,
+        Width,
+        Height
+    }
+
+    public FitType fitType;
+
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+
+
+    public override void CalculateLayoutInputHorizontal() {
+
+        base.CalculateLayoutInputHorizontal();
+
+        float sqrRt = Mathf.Sqrt(transform.childCount);
+        rows = Mathf.CeilToInt(sqrRt);
+        columns = Mathf.CeilToInt(sqrRt);
+
+        if (fitType == FitType.Width)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+        if (fitType == FitType.Height)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * 2) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)rows) * 2) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = cellWidth;
+        cellSize.y = cellHeight;
+
+        int columnCount = 0;
+        int rowCount = 0;
+
+        for (int i = 0; i < rectChildren.Count; i++) {
+            
+            rowCount = i / columns;
+            columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+
+    }
+
+    public override void CalculateLayoutInputVertical() {
+
+    }
+
+    public override void SetLayoutHorizontal() {
+
+    }
+
+    public override void SetLayoutVertical() {
+
+    }
+}
